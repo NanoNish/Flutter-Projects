c0640e45efe37546e662690d6b6830c676fbd348
NanoNish <nishkarsh1206@gmail.com>
2022-03-19 18:33:04 +0530

    Flutter Projects + MDG Assignment Done

---
+++ parth_saanp/android/app/src/main/kotlin/com/example/parth_saanp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.parth_saanp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ parth_saanp/lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:shake/shake.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  bool _state = true;
+
+  @override
+  void initState() {
+    super.initState();
+    ShakeDetector detector = ShakeDetector.autoStart(onPhoneShake: () {
+      setState(() {
+        _state = !_state;
+      });
+      //print(_state);  //for testing purposes
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(children: [
+        Container(
+          color: _state == true ? Colors.black : Colors.red,
+          height: 400,
+          width: 500,
+        ),
+        Container(
+          color: _state == true ? Colors.red : Colors.black,
+          height: 400,
+          width: 500,
+        ),
+      ]),
+    );
+  }
+}
